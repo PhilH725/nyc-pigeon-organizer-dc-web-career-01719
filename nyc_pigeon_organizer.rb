883556717c2816bf1fec5a66ab1f323b9b3c5cd0
Phil Hartley <philh725@gmail.com>
2019-01-01 16:48:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
       pidgeon_name.each do |i|
         if not pidgeon_hash[i]
           pidgeon_hash[i] = {:color => [], :gender => [], :lives => []}
-          pidgeon_hash[i][attribute] = [symbol]
+          pidgeon_hash[i][attribute] = [symbol.to_s]
         else
           pidgeon_hash[i].each do |key, value_array|
             if key == attribute
