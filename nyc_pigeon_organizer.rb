93500f51d6a1abf2f433232ad0a8ff1782c9dae8
Phil Hartley <philh725@gmail.com>
2019-01-01 16:39:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,5 @@
+require pry
+
 def nyc_pigeon_organizer(data)
   # write your code here
   
@@ -5,6 +7,7 @@ def nyc_pigeon_organizer(data)
   element = {:color => [], :gender => [], :lives => []}
   data.each do |attribute, att_value|
     att_value.each do |symbol, pidgeon_name|
+      binding.pry
       pidgeon_hash[pidgeon_name] = {attribute => symbol}
     end
   end
