3633053f51739be54f9260bc2bacb962a9d5c18a
Phil Hartley <philh725@gmail.com>
2019-01-01 16:37:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,10 +5,7 @@ def nyc_pigeon_organizer(data)
   element = {:color => [], :gender => [], :lives => []}
   data.each do |attribute, att_value|
     att_value.each do |symbol, pidgeon_name|
-      puts pidgeon_name
-      #if not pidgeon_hash[pidgeon_name]
-        #pidgeon_hash[pidgeon_name] = {:color => [1], :gender => [], #:lives => []}
-      #end
+      pidgeon_hash[pidgeon_name][attribute] = symbol
     end
   end
   puts pidgeon_hash
